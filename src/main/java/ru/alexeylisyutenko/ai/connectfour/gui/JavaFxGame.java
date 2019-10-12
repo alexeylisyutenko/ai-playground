80d3abdb73ee44b7cf1831d97d6636c014c703e9
Alexey Lisyutenko <alis@icbcom.ru>
2019-10-12 20:55:42 +0300

    Make JavaFX application possible run with gradle

--- src/main/java/ru/alexeylisyutenko/ai/connectfour/gui/JavaFxGame.java
+++ src/main/java/ru/alexeylisyutenko/ai/connectfour/gui/JavaFxGame.java
@@ -1,12 +1,17 @@
 package ru.alexeylisyutenko.ai.connectfour.gui;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class JavaFxGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene( new StackPane(new Label("JavaFX 11")), 300, 200);
         primaryStage.setTitle("Connect Four Game");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
