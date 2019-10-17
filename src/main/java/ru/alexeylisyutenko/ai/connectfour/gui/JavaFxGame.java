e8464139ccd95d08b49c3be5cfdd7d8ba54fd2a2
Alexey Lisyutenko <alis@icbcom.ru>
2019-10-18 02:20:27 +0300

    Continue JavaFX GUI development. Add demo animation.

--- src/main/java/ru/alexeylisyutenko/ai/connectfour/gui/JavaFxGame.java
+++ src/main/java/ru/alexeylisyutenko/ai/connectfour/gui/JavaFxGame.java
@@ -1,169 +1,147 @@
 package ru.alexeylisyutenko.ai.connectfour.gui;
 
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.*;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import org.apache.commons.lang3.RandomUtils;
+
+import static ru.alexeylisyutenko.ai.connectfour.game.Constants.BOARD_HEIGHT;
+import static ru.alexeylisyutenko.ai.connectfour.game.Constants.BOARD_WIDTH;
 
 public class JavaFxGame extends Application {
+    private final static int SCENE_WIDTH = 1024;
+    private final static int SCENE_HEIGHT = 768;
 
-    private final static int FRAME_WIDTH = 420;
-    private final static int FRAME_HEIGHT = 360;
+    private final static int TOKEN_RADIUS = 20;
+    private final static int TOKEN_MARGIN = 10;
+    private final static int GRID_OUTER_MARGIN = 10;
+
+    private final static int CELL_SIZE = (TOKEN_RADIUS + TOKEN_MARGIN) * 2;
+    private final static int GRID_WIDTH = CELL_SIZE * BOARD_WIDTH + GRID_OUTER_MARGIN * 2;
+    private final static int GRID_HEIGHT = CELL_SIZE * BOARD_HEIGHT + GRID_OUTER_MARGIN * 2;
+
+    private Pane tokenPane;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Connect Four Game");
-
-        StackPane p = new StackPane();
-
-        Group group = createCanvasGroup();
-        p.getChildren().add(group);
-        StackPane.setAlignment(group, Pos.CENTER);
-
-        primaryStage.setScene(new Scene(p, 1024, 768));
+        primaryStage.setScene(new Scene(createContent(), SCENE_WIDTH, SCENE_HEIGHT));
         primaryStage.show();
     }
 
-    private Group createCanvasGroup() {
-        Group group = new Group();
-        Canvas canvas = new Canvas(FRAME_WIDTH, FRAME_HEIGHT);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+    private Parent createContent() {
+        StackPane root = new StackPane();
 
-        drawFrame(gc);
+        Pane tokenPane = new Pane();
+        tokenPane.setMaxWidth(GRID_WIDTH);
+        tokenPane.setMaxHeight(GRID_HEIGHT);
+        tokenPane.setMinWidth(GRID_WIDTH);
+        tokenPane.setMinHeight(GRID_HEIGHT);
 
-        DropShadow dropShadow = new DropShadow();
-        dropShadow.setRadius(5.0);
-        dropShadow.setOffsetX(3.0);
-        dropShadow.setOffsetY(3.0);
-        dropShadow.setColor(Color.color(0.4, 0.5, 0.5));
-        canvas.setEffect(dropShadow);
+//        addDemoTokens(tokenPane);
+        root.getChildren().add(tokenPane);
+        StackPane.setAlignment(tokenPane, Pos.CENTER);
 
-        drawTokenInBoardCoordinates(5, 0, 1, gc);
-        drawTokenInBoardCoordinates(4, 0, 1, gc);
-        drawTokenInBoardCoordinates(3, 0, 1, gc);
-        drawTokenInBoardCoordinates(5, 2, 2, gc);
-        drawTokenInBoardCoordinates(5, 3, 2, gc);
-        drawTokenInBoardCoordinates(5, 4, 1, gc);
-        drawTokenInBoardCoordinates(5, 5, 2, gc);
-        drawTokenInBoardCoordinates(4, 3, 1, gc);
-        drawTokenInBoardCoordinates(4, 4, 1, gc);
-        drawTokenInBoardCoordinates(3, 4, 2, gc);
-        drawTokenInBoardCoordinates(5, 6, 2, gc);
+        Shape grid = createGrid();
+        root.getChildren().add(grid);
+        StackPane.setAlignment(grid, Pos.CENTER);
 
-        group.getChildren().add(canvas);
-        return group;
+        // Start demo animation.
+        root.setOnMouseClicked(event -> {
+            int column = RandomUtils.nextInt(0, BOARD_WIDTH);
+
+            Color color;
+            boolean red = RandomUtils.nextBoolean();
+            if (red) {
+                color = Color.RED;
+            } else {
+                color = Color.DARKORANGE;
+            }
+
+            Token token = new Token(color);
+            token.setCenterX(GRID_OUTER_MARGIN + TOKEN_MARGIN + TOKEN_RADIUS + CELL_SIZE * column);
+            token.setCenterY(GRID_OUTER_MARGIN + TOKEN_MARGIN + TOKEN_RADIUS - CELL_SIZE);
+            tokenPane.getChildren().add(token);
+
+            TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5), token);
+            translateTransition.setToY(6 * CELL_SIZE);
+            translateTransition.play();
+        });
+
+        return root;
     }
 
-    void drawTokenInBoardCoordinates(int row, int column, int playerId, GraphicsContext gc) {
-        if (row < 0 || row > 5) {
-            throw new IllegalArgumentException("Incorrect row value");
-        }
-        if (column < 0 || column > 6) {
-            throw new IllegalArgumentException("Incorrect column value");
-        }
+    private Shape createGrid() {
+        Shape shape = createGridShape();
+        shape.setFill(Color.DARKBLUE);
 
-        double x = 10 + column * 60;
-        double y = 10 + row * 60;
+        Light.Distant light = new Light.Distant();
+        light.setAzimuth(225.0);
+        light.setElevation(45.0);
+        Lighting lighting = new Lighting(light);
+        lighting.setSurfaceScale(0.5);
+        lighting.setSpecularConstant(0.8);
+        shape.setEffect(lighting);
 
-        if (playerId == 1) {
-            drawRedToken(x, y, gc);
-        } else {
-            drawYellowToken(x, y, gc);
-        }
+        return shape;
     }
 
-    private void drawRedToken(double x, double y, GraphicsContext gc) {
-//        Color lighterColor = Color.rgb(254, 193, 193);
-//        Color baseColor = Color.rgb(200, 0, 0);
-        Color lighterColor = Color.RED;
-        Color baseColor = Color.DARKRED;
-
-        drawToken(x, y, baseColor, lighterColor, gc);
-    }
-
-    private void drawYellowToken(double x, double y, GraphicsContext gc) {
-        Color lighterColor = Color.LIGHTYELLOW;
-        Color baseColor = Color.DARKORANGE;
-
-//        Color lighterColor = Color.rgb(242, 240, 210);
-//        Color baseColor = Color.rgb(178, 170, 48);
-
-        drawToken(x, y, baseColor, lighterColor, gc);
-    }
-
-    private void drawToken(double x, double y, Color baseColor, Color lighterColor, GraphicsContext gc) {
-        Stop[] stops;
-        LinearGradient gradient;
-
-        // Outer circle
-        stops = new Stop[]{new Stop(0, lighterColor), new Stop(1, baseColor)};
-        gradient = new LinearGradient(0.5, 0, 0.5, 1, true, CycleMethod.NO_CYCLE, stops);
-        gc.setFill(gradient);
-        gc.fillOval(x, y, 40, 40);
-        gc.fill();
-        gc.stroke();
-
-        // Inner circle
-        stops = new Stop[]{new Stop(0, baseColor), new Stop(1, lighterColor)};
-        gradient = new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE, stops);
-        gc.setFill(gradient);
-        gc.fillOval(x + 3, y + 3, 34, 34);
-        gc.fill();
-        gc.stroke();
-    }
-
-    private void drawFrame(GraphicsContext gc) {
-        Color frameColor = Color.DARKBLUE;
-
-        // Square
-        LinearGradient lg = new LinearGradient(0, 0, 1, 1, true,
-                CycleMethod.NO_CYCLE,
-                new Stop(0.0, Color.BLUE),
-                new Stop(1.0, frameColor));
-
-
-        gc.beginPath();
-        gc.setFill(lg);
-        gc.fillRoundRect(0, 0, FRAME_WIDTH, FRAME_HEIGHT, 30, 30);
-        gc.fill();
-
-        // Empty slots
-        for (int row = 0; row < 6; row++) {
-            for (int column = 0; column < 7; column++) {
-                double x = 10 + column * 60;
-                double y = 10 + row * 60;
-                drawEmptySlot(x, y, gc);
+    private Shape createGridShape() {
+        Rectangle rectangle = new Rectangle(GRID_WIDTH, GRID_HEIGHT);
+        rectangle.setArcHeight(30.0);
+        rectangle.setArcWidth(30.0);
+        Shape shape = rectangle;
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            for (int column = 0; column < BOARD_WIDTH; column++) {
+                Circle circle = new Circle(TOKEN_RADIUS);
+                circle.setCenterX(GRID_OUTER_MARGIN + TOKEN_MARGIN + TOKEN_RADIUS + column * CELL_SIZE);
+                circle.setCenterY(GRID_OUTER_MARGIN + TOKEN_MARGIN + TOKEN_RADIUS + row * CELL_SIZE);
+                shape = Shape.subtract(shape, circle);
             }
         }
+        return shape;
     }
 
-    private void drawEmptySlot(double x, double y, GraphicsContext gc) {
-        Stop[] stops;
-        LinearGradient gradient;
+    private void addDemoTokens(Pane tokenPane) {
+        Token token = new Token(Color.RED);
+        token.setCenterX(GRID_OUTER_MARGIN + TOKEN_MARGIN + TOKEN_RADIUS);
+        token.setCenterY(GRID_OUTER_MARGIN + TOKEN_MARGIN + TOKEN_RADIUS);
+        tokenPane.getChildren().add(token);
 
-        // Circle shadow
-        gc.beginPath();
-        stops = new Stop[]{new Stop(0, Color.GRAY), new Stop(1, Color.WHITE)};
-        gradient = new LinearGradient(0, 0, 1, 1, true, CycleMethod.NO_CYCLE, stops);
-        gc.setFill(gradient);
-        gc.fillOval(x, y, 40, 40);
-        gc.fill();
-        gc.setStroke(Color.WHITE);
-
-        // Circle
-        Color circleColor = Color.rgb(217, 217, 255);
-        gc.setFill(circleColor);
-        gc.fillOval(x + 2, y + 2, 38, 38);
-        gc.fill();
-        gc.setStroke(Color.WHITE);
-        gc.stroke();
+        Token token2 = new Token(Color.DARKORANGE);
+        token2.setCenterX(GRID_OUTER_MARGIN + TOKEN_MARGIN + TOKEN_RADIUS + CELL_SIZE);
+        token2.setCenterY(GRID_OUTER_MARGIN + TOKEN_MARGIN + TOKEN_RADIUS);
+        tokenPane.getChildren().add(token2);
     }
 
+    private static class Token extends Circle {
+        public Token(Color color) {
+            super(TOKEN_RADIUS, color);
+            setupLighting();
+        }
+
+        private void setupLighting() {
+            Light.Distant light = new Light.Distant();
+            light.setAzimuth(225.0);
+            light.setElevation(55.0);
+            Lighting lighting = new Lighting(light);
+            lighting.setSurfaceScale(1.5);
+            setEffect(lighting);
+        }
+    }
 
 }
