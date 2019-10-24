b9f1b3c7f4f2f7e8aedd441110285cc757ce0717
Alexey Lisyutenko <alis@icbcom.ru>
2019-10-24 21:15:59 +0300

    Start naive GUI game development

--- src/main/java/ru/alexeylisyutenko/ai/connectfour/gui/JavaFxGame.java
+++ src/main/java/ru/alexeylisyutenko/ai/connectfour/gui/JavaFxGame.java
@@ -4,31 +4,38 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.*;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.apache.commons.lang3.RandomUtils;
+import ru.alexeylisyutenko.ai.connectfour.game.*;
+import ru.alexeylisyutenko.ai.connectfour.player.RandomPlayer;
 
 import static ru.alexeylisyutenko.ai.connectfour.game.Constants.BOARD_HEIGHT;
 import static ru.alexeylisyutenko.ai.connectfour.game.Constants.BOARD_WIDTH;
 
 public class JavaFxGame extends Application {
 
+    private BoardControl boardControl;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Connect Four Game");
         primaryStage.setScene(new Scene(createContent(), BoardControl.SCENE_WIDTH, BoardControl.SCENE_HEIGHT));
         primaryStage.show();
+
+        //!!!
+        startGame();
     }
 
     private Parent createContent() {
         StackPane root = new StackPane();
-        BoardControl boardControl = new BoardControl();
-        boardControl.addEventHandler(BoardControl.ColumnClickEvent.COLUMN_CLICKED, event -> {
-            int row = RandomUtils.nextInt(0, BOARD_HEIGHT);
-            int column = event.getColumn();
-            boardControl.displayTokenWithAnimation(row, column, randomTokenColor());
-        });
+        boardControl = new BoardControl();
+//        boardControl.addEventHandler(BoardControl.ColumnClickEvent.COLUMN_CLICKED, event -> {
+//            int row = RandomUtils.nextInt(0, BOARD_HEIGHT);
+//            int column = event.getColumn();
+//            boardControl.displayTokenWithAnimation(row, column, randomTokenColor());
+//        });
         root.getChildren().add(boardControl);
         StackPane.setAlignment(boardControl, Pos.CENTER);
         return root;
@@ -43,6 +50,83 @@ public class JavaFxGame extends Application {
             color = Color.DARKORANGE;
         }
         return color;
+    }
+
+    private void startGame() {
+        Player player1 = new RandomPlayer();
+        Player player2 = new RandomPlayer();
+        GameRunner gameRunner = new DefaultGameRunner(player1, player2, new GuiGameEventListener());
+        gameRunner.startGame();
+    }
+
+    private void displayBoard(Board board) {
+        boardControl.hideAll();
+        for (int row = 0; row < BOARD_HEIGHT; row++) {
+            for (int column = 0; column < BOARD_WIDTH; column++) {
+                int cellPlayerId = board.getCellPlayerId(row, column);
+                if (cellPlayerId == 1) {
+                    boardControl.displayToken(row, column, Color.RED);
+                } else if (cellPlayerId == 2) {
+                    boardControl.displayToken(row, column, Color.DARKORANGE);
+                }
+            }
+        }
+    }
+
+    private class GuiPlayer implements Player {
+        @Override
+        public void gameStarted(int playerId) {
+            // Do nothing
+        }
+
+        @Override
+        public void requestMove(GameContext gameContext) {
+            // Wait for a click event? How???
+        }
+
+        @Override
+        public void gameFinished(GameResult gameResult) {
+
+        }
+    }
+
+    private class GuiGameEventListener implements GameEventListener {
+        @Override
+        public void gameStarted(GameRunner gameRunner, Board board) {
+            displayBoard(board);
+        }
+
+        @Override
+        public void illegalMoveAttempted(GameRunner gameRunner, int playerId, int column, Board board) {
+
+        }
+
+        @Override
+        public void moveRequested(GameRunner gameRunner, int playerId, Board board) {
+            System.out.println("Move requested: " + playerId);
+        }
+
+        @Override
+        public void moveMade(GameRunner gameRunner, int playerId, int column, Board board) {
+            System.out.println("Move made: playerid = " + playerId + " column = " + column);
+
+            Color tokenColor;
+            if (playerId == 1) {
+                tokenColor = Color.RED;
+            } else {
+                tokenColor = Color.DARKORANGE;
+            }
+            int heightOfColumn = board.getHeightOfColumn(column);
+            int row = heightOfColumn - 1;
+
+            displayBoard(board);
+//            boardControl.displayToken(row, column, tokenColor);
+        }
+
+        @Override
+        public void gameFinished(GameRunner gameRunner, GameResult gameResult) {
+            System.out.println("Game finished: " + gameResult);
+        }
     }
 
 }
